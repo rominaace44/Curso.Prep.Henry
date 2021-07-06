0370a5e55298c677108ef36ac839fca8e4aae6e6
rominaace44 <rominaace44@gmail.com>
2021-07-06 15:49:34 -0300

    ejercicios

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -134,76 +134,99 @@ function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
 	// Tu código:
-	resultado = Math.pow(num,2)
-	return resultado
+	
+	return Math.pow(num,2)
 	
 }
 
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
-  // Tu código:
+	// Tu código:
+	return Math.pow(num,3)
+
 }
 
 function elevar(num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
-  // Tu código:
+	// Tu código:
+	return Math.pow(num, exponent)
 }
 
 function redondearNumero(num) {
   // Redondea "num" al entero más próximo y devuélvelo
-  // Tu código:
+	// Tu código:
+	return Math.round(num)
 }
 
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
-  // Tu código:
+	// Tu código:
+	return Math.ceil(num)
 }
 
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
-  //Pista: investigá qué hace el método Math.random()
+	//Pista: investigá qué hace el método Math.random()
+	return Math.random(0,1)
 }
 
 function esPositivo(numero) {
   //La función va a recibir un entero. Devuelve como resultado una cadena de texto que indica si el número es positivo o negativo. 
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
-  //Si el número es 0, devuelve false
+	//Si el número es 0, devuelve false
+	if(numero > 0 ){
+		return "Es positivo"
+	}
+	if (numero < 0){
+		return "Es negativo"
+	}
+	if (numero == 0){
+		return false
+	}
 }
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
-  // Tu código:
+	// Tu código:
+	nuevo = str +"!"
+	return nuevo
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
-  // Tu código:
+	// Tu código:
+	return nombre + apellido
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
-  // Tu código:
+	// Tu código:
+	return "Hola " + nombre
+	
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
-  // Tu código:
+	// Tu código:
+	return alto * ancho
 }
 
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
-  //Escribe tu código aquí
+	//Escribe tu código aquí
+	return lado *4
 }
 
 
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
-  //Escribe tu código aquí
+	//Escribe tu código aquí
+	return (base * altura)/2
 
 }
 
@@ -212,7 +235,7 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  
+  return euro * 1.20
 }
 
 
@@ -220,7 +243,16 @@ function esVocal(letra){
   //Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje “Es vocal”. 
   //Verificar si el usuario ingresó un string de más de un carácter y, en ese caso, informarle 
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
-  //Escribe tu código aquí
+	//Escribe tu código aquí
+	if(letra.length >1){
+		return "dato incorrecto"
+	}
+	if(letra==a) return "es vocal"
+	if(letra==e) return "es vocal"
+	if(letra==i) return "es vocal"
+	if(letra==o) return "es vocal"
+	if(letra==u) return "es vocal"
+
 }
 
 
